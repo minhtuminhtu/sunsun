0db7351365c8ca28027dd3576221de4a1ec5a1c5
tranvanthanh866 <tranvanthanh866@gmail.com>
2019-10-16 17:41:39 +0700

    build folder view

---
+++ resources/views/sunsun/front/index.blade.php
@@ -0,0 +1,19 @@
+@extends('sunsun.front.template')
+
+@section('head')
+    @parent
+
+@endsection
+
+@section('main')
+
+<main id="mainArea">
+    MAIN
+</main>
+
+@endsection
+
+@section('script')
+    @parent
+
+@endsection
---
+++ resources/views/sunsun/template.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang='{{ str_replace('_', '-', app()->getLocale()) }}'>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='csrf-token' content='{{ csrf_token() }}'>
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+        @yield('head')
+
+    </head>
+    <body>
+		@yield('beforebody')
+
+		@yield('header')
+
+		@yield('main')
+
+		@yield('footer')
+
+        @yield('script')
+
+    </body>
+</html>
