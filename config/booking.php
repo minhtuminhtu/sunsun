962134da0778f10ab77959cbd75f38128cdbf143
tranvanthanh866 <tranvanthanh866@gmail.com>
2019-10-17 14:19:54 +0700

    config booking

---
+++ config/booking.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'user' => [
+        'label' => '【ご利用】',
+        'options' => [
+            'no' => 'はじめて​',
+            'yes' => '​リピート​',
+        ]
+    ],
+    'transportation' => [
+        'label' => '【交通手段】​',
+        'options' => [
+            'car' => '車​',
+            'bus' => '​バス​',
+        ]
+    ]
+];
