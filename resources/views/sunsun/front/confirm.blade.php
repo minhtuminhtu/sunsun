ccbad46314b4295a543594dc095c470532d430d3
tranvanthanh866 <tranvanthanh866@gmail.com>
2019-10-18 09:43:59 +0700

    merge

--- resources/views/sunsun/front/confirm.blade.php
+++ resources/views/sunsun/front/confirm.blade.php
@@ -13,20 +13,20 @@
                 <div class="col-sm-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 col-xl-4 offset-xl-4 pb-3 border-left border-bottom border-right">
                     <p class="text-md-left mt-2 mb-0">≪交通手段≫</p>
                     @if($data['transportation'] == "car")
-                        <p class="text-md-left pl-4 mb-0">{{ config('booking.transportation.options.car') }}</p>     
-                    @else 
-                        <p class="text-md-left pl-4 mb-0">{{ config('booking.transportation.options.bus')  }} 洲本IC着：{{ $data['bus_arrival']  }}</p> 
-                        <p class="text-md-left pl-4 mb-0">送迎：{{ $data['pick_up']  }}</p> 
+                        <p class="text-md-left pl-4 mb-0">{{ config('booking.transportation.options.car') }}</p>
+                    @else
+                        <p class="text-md-left pl-4 mb-0">{{ config('booking.transportation.options.bus')  }} 洲本IC着：{{ $data['bus_arrival']  }}</p>
+                        <p class="text-md-left pl-4 mb-0">送迎：{{ $data['pick_up']  }}</p>
                     @endif
-   
+
                     <p class="text-md-left mt-2 mb-0">≪選択コース≫</p>
-                    <p class="text-md-left font-weight-bold pt-2 pl-2 mb-0 bg-warning">{{ $data['services']  }}</p>     
+                    <p class="text-md-left font-weight-bold pt-2 pl-2 mb-0 bg-warning">{{ $data['services']  }}</p>
 
                     @if($data['used'] == "yes")
-                        <p class="text-md-left pl-4 mb-0 bg-warning">ご利用回数： {{ config('booking.used.options.yes') }}</p>     
-                    @else 
-                        <p class="text-md-left pl-4 mb-0 bg-warning">ご利用回数： {{ config('booking.used.options.no') }}</p> 
-                        <p class="text-md-left pl-4 mb-0 bg-warning">※開始時間の15分前までにお越しください。</p> 
+                        <p class="text-md-left pl-4 mb-0 bg-warning">ご利用回数： {{ config('booking.used.options.yes') }}</p>
+                    @else
+                        <p class="text-md-left pl-4 mb-0 bg-warning">ご利用回数： {{ config('booking.used.options.no') }}</p>
+                        <p class="text-md-left pl-4 mb-0 bg-warning">※開始時間の15分前までにお越しください。</p>
                     @endif
 
                     <p class="text-md-left pl-4 mb-0 bg-warning">{{ $data['sex'] }} : {{ $data['age'] }}歳</p>
@@ -34,7 +34,7 @@
                     <p class="text-md-left pl-4 mb-0 bg-warning">［オプション］</p>
 
                     <p class="text-md-left pl-5 mb-0 bg-warning">昼食: {{ $data['lunch'] }}</p>
-                    
+
 
 
                     <div class="row mt-5">
@@ -54,19 +54,6 @@
 
 @section('script')
     @parent
-    <script src="{{asset('sunsun/lib/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js')}}"></script>
-    <script>
-        /**
-         * Japanese translation for bootstrap-datepicker
-         * Norio Suzuki <https://github.com/suzuki/>
-         */
 
-        $(function() {
-            $('#datetimepicker').datetimepicker({
-                pickTime: false,
-                locale: 'ja',
-            });
-        });
-    </script>
 @endsection
 
