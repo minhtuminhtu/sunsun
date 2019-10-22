c0c9e1b13e6ede4b3c9717fddaa6a50f835af31a
MinhTu <minhtu.bk14@gmail.com>
2019-10-22 10:21:39 +0700

    fix confirm config

--- resources/views/sunsun/front/confirm.blade.php
+++ resources/views/sunsun/front/confirm.blade.php
@@ -14,7 +14,7 @@
 
                     <p class="text-md-left mt-2 mb-0">≪交通手段≫</p>
 
-                    @if($data['transportation'] == "車​")
+                    @if($data['transportation'] == config('booking.transportation.options.bus') )
                         <p class="text-md-left pl-4 mb-0">{{ $data['transportation']??'' }}</p>
                     @else
                         <p class="text-md-left pl-4 mb-0">{{ $data['transportation'] }} 洲本IC着：{{ $data['bus_arrival']  }}</p>
@@ -25,22 +25,22 @@
 
 
                     <div class="booking-info pt-2 pb-2">
-                        @if($data['services'] == "酵素部屋貸切プラン")
+                        @if($data['services'] == config('booking.services.options.eat'))
                             <p class="text-md-left font-weight-bold pl-2 mb-0 ">酵素部屋1部屋貸切プラン</p>
-                        @elseif($data['services'] == "断食プラン")
+                        @elseif($data['services'] == config('booking.services.options.no'))
                             <p class="text-md-left font-weight-bold pl-2 mb-0 ">断食プラン</p>
                         @else
                             <p class="text-md-left font-weight-bold pl-2 mb-0 ">{{ $data['services']  }}</p>
                         @endif
 
-                        @if($data['services'] == "1日リフレッシュプラン")
+                        @if($data['services'] == config('booking.services.options.day'))
                             <p class="text-md-left pl-2 mb-0 ">［酵素風呂2回とお食事付き］</p>
                         @endif
 
 
 
-                        @if($data['services'] != "ペット酵素浴")
-                            @if($data['used'] == "​リピート​")
+                        @if($data['services'] != config('booking.services.options.pet'))
+                            @if($data['used'] == config('booking.used.options.yes'))
                                 <p class="text-md-left pl-4 mb-0 ">ご利用回数： {{ $data['used'] }}</p>
                             @else
                                 <p class="text-md-left pl-4 mb-0 ">ご利用回数： {{ $data['used'] }}</p>
@@ -50,11 +50,11 @@
 
 
 
-                        @if(($data['services'] == "酵素浴") || ($data['services'] == "断食プラン") || ($data['services'] == "1日リフレッシュプラン"))
+                        @if(($data['services'] == config('booking.services.options.normal')) || ($data['services'] == config('booking.services.options.no')) || ($data['services'] == config('booking.services.options.day')))
                             <p class="text-md-left pl-4 mb-0 ">{{ $data['sex'] }} : {{ $data['age'] }}歳</p>
                         @endif
 
-                        @if($data['services'] == "断食プラン")
+                        @if($data['services'] == config('booking.services.options.no'))
                             <p class="text-md-left pl-4 mb-0 font-weight-bold">利用期間</p>
                             <p class="text-md-left pl-5 mb-0 ">開始日：{{ $data['plan_date_start'] }}</p>
                             <p class="text-md-left pl-5 mb-0 ">終了日：{{ $data['plan_date_end'] }}</p>
@@ -70,45 +70,45 @@
 
 
 
-                        @if($data['services'] == "酵素浴")
+                        @if($data['services'] == config('booking.services.options.normal'))
                             <p class="text-md-left pl-4 mb-0 ">{{ $data['date'] }}  {{ $data['time'] }}</p>
-                        @elseif($data['services'] == "1日リフレッシュプラン")
+                        @elseif($data['services'] == config('booking.services.options.day'))
                             <p class="text-md-left pl-4 mb-0 ">{{ $data['date'] }}</p>
                             <p class="text-md-left pl-5 mb-0 ">入浴1回目 {{ $data['time1'] }}</p>
                             <p class="text-md-left pl-5 mb-0 ">入浴2回目 {{ $data['time2'] }}</p>
-                        @elseif(($data['services'] == "酵素部屋貸切プラン") || ($data['services'] == "ペット酵素浴"))
+                        @elseif(($data['services'] == config('booking.services.options.eat')) || ($data['services'] == config('booking.services.options.pet')))
                             <p class="text-md-left pl-4 mb-0 ">{{ $data['date'] }}</p>
                             <p class="text-md-left pl-5 mb-0 ">{{ $data['time_room']??'' }}</p>
                         @endif
 
-                        @if($data['services'] == "ペット酵素浴")
+                        @if($data['services'] == config('booking.services.options.pet'))
                             <p class="text-md-left pl-4 mb-0 ">ペット数：{{ $data['number_pet']??'' }}</p>
                             <p class="text-md-left pl-4 mb-0 ">ペット種類：{{ $data['pet_type']??'' }}</p>
                         @endif
 
 
 
-                        @if($data['services'] != "ペット酵素浴")
+                        @if($data['services'] != config('booking.services.options.pet'))
                             <p class="text-md-left pl-4 mb-0 ">［オプション］</p>
                         @endif
 
 
-                        @if($data['services'] != "ペット酵素浴")
-                            @if($data['services'] != "断食プラン")
-                                @if($data['services'] == "酵素部屋貸切プラン")
-                                    @if($data['number_lunch_book'] != "無し")
+                        @if($data['services'] != config('booking.services.options.pet'))
+                            @if($data['services'] != config('booking.services.options.no'))
+                                @if($data['services'] == config('booking.services.options.eat'))
+                                    @if($data['number_lunch_book'] != config('booking.number_lunch_book.options.no'))
                                         <p class="text-md-left pl-5 mb-0">昼食: {{ $data['number_lunch_book']??'' }}</p>
                                     @endif
-                                @elseif($data['services'] != "1日リフレッシュプラン")
-                                    @if($data['lunch'] != "無し")
+                                @elseif($data['services'] != config('booking.services.options.day'))
+                                    @if($data['lunch'] != config('booking.lunch.options.no'))
                                         <p class="text-md-left pl-5 mb-0">昼食: {{ $data['lunch']??'' }}</p>
                                     @endif
                                 @endif
                             @endif
-                            @if($data['pet'] == "追加する")
+                            @if($data['pet'] == config('booking.pet.options.yes'))
                                 <p class="text-md-left pl-5 mb-0">ペット預かり</p>
                             @endif
-                            @if($data['room'] != "無し")
+                            @if($data['room'] != config('booking.room.options.no'))
                                 <p class="text-md-left pl-5 mb-0">宿泊：有り</p>
                                 <p class="text-md-left pl-5 mb-0">部屋ﾀｲﾌﾟ：{{ $data['room']??'' }}</p>
                                 <p class="text-md-left pl-5 mb-0">宿泊人数：{{ $data['number_guests_stay']??'' }}</p>
