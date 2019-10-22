cf10f3ef32495c0e7abf5f66c2809ef71f391f0e
tranvanthanh866 <tranvanthanh866@gmail.com>
2019-10-22 13:45:55 +0700

    merge

---
+++ config/data_tmp.php
@@ -0,0 +1,480 @@
+<?php
+return [
+    'time_room' => [
+        [
+            'time' => '9:45',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+
+        ],
+        [
+            'time' => '10:15',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '10:45',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '13:15',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '13:45',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '14:15',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '15:15',
+            'status' => '1',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '15:45',
+            'status' => '0',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '16:15',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '17:45',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '18:15',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '18:45',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+
+    ],
+    'room' => [
+        [
+            'time_from' => '9:45',
+            'time_to' => '12:00',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+
+        ],
+        [
+            'time' => '13:15',
+            'time_to' => '15:00',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '15:15',
+            'time_to' => '17:00',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+
+    ],
+    'room_pet' => [
+        [
+            'time_from' => '9:30',
+            'time_to' => '10:30',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+
+        ],
+        [
+            'time' => '11:00',
+            'time_to' => '12:00',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '14:00',
+            'time_to' => '15:00',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+        [
+            'time' => '15:30',
+            'time_to' => '16:30',
+            'quantity' => '3',
+            'room1' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ① ",
+            ],
+            'room2' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room3' => [
+                'status' => '0',
+                'quantity' => '3',
+                'name' => "ベッド <br> ②",
+            ],
+            'room4' => [
+                'status' => '1',
+                'quantity' => '3',
+                'name' => "ベッド <br> ④",
+            ],
+        ],
+
+    ]
+];
