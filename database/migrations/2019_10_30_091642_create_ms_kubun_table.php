b2d6c58e6ca0090fb68a2073254b4cfeb31c339b
tranvanthanh866 <tranvanthanh866@gmail.com>
2019-10-31 10:34:08 +0700

    upload migrate

--- database/migrations/2019_10_30_091642_create_ms_kubun_table.php
+++ database/migrations/2019_10_30_091642_create_ms_kubun_table.php
@@ -14,7 +14,7 @@ class CreateMsKubunTable extends Migration
     public function up()
     {
         Schema::create('ms_kubun', function (Blueprint $table) {
-            $table->string('kubun_type',3);
+            $table->string('kubun_type',3)->comment('primary key (kubun_type,kubun_id)');
             $table->string('kubun_id',2);
             $table->string('kubun_value',255);
             $table->integer('sort_no')->comment('orderBy');
