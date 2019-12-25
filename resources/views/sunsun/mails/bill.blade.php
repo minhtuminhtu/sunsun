cf5667b5a16ab52a34cbb4b18e2b40f3a960fc53
MinhTu <minhtu.bk14@gmail.com>
2019-12-25 15:14:05 +0700

    add mail

--- resources/views/sunsun/mails/bill.blade.php
+++ resources/views/sunsun/mails/bill.blade.php
@@ -21,8 +21,8 @@
         }
         
         p {
-            Margin: 0px !important;
-            Padding: 0px !important;
+            margin: 0px !important;
+            padding: 0px !important;
         }
         
         table {
@@ -108,92 +108,96 @@
                 width: 100vw !important;
             }
         }
+        
+        .head_img{
+            margin-top: 5vh!important;
+            margin-left: 5vw!important;
+            margin-right: 5vw!important;
+        }
+        .img-fluid_head{
+            width: 7rem;
+            max-width: 7rem !important;
+            height: auto!important;
+        }
+        .order_status{
+            margin-top: 10vh!important;
+            text-align: center!important;
+            color: #028720!important;
+        }
+        .order_status_image{
+            width: 4rem;
+            max-width:  4rem important;
+            height: auto!important;
+        }
+        .order_status_title{
+            font-family:'Open Sans', Arial, sans-serif!important;
+            font-size:18px!important;
+            line-height:22px!important;
+            margin-top: 1vh!important;
+        }
+        .content_block{
+            margin-top: 5vh!important;
+            display: block!important;
+            justify-content: center!important;
+        }
+        .content_text{
+            text-align: center!important;
+            font-family:'Open Sans', Arial, sans-serif!important;
+            font-size:18px!important;
+            line-height: 150%!important;
+        }
+        .bg-color-01 {
+            background-color: #513e29!important;
+            padding: 1vh 0!important;
+            text-align: center!important;
+        }
+        .fz-1_2 {
+            font-size: 0.75rem!important;
+        }
+        .text-white {
+            color: #fff!important;
+        }
+        .footer {
+           position: fixed!important;
+           left: 0!important;
+           bottom: 0!important;
+           width: 100%!important;
+           color: white!important;
+           text-align: center!important;
+        }
+        
     </style>
 </head>
 
-<body class="em_body" style="margin:0px; padding:0px;" bgcolor="#efefef">
-    <table class="em_full_wrap" valign="top" width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#efefef" align="center">
-        <tbody>
-            <tr>
-                <td valign="top" align="center">
-                    <table class="em_main_table" style="width:700px;" width="700" cellspacing="0" cellpadding="0" border="0" align="center">
-                        <!--Header section-->
-                        <tbody>
-                            <tr>
-                                <td style="padding:15px;" class="em_padd" valign="top" bgcolor="#fff" align="center">
-                                    <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
-                                        <tbody>
-                                            <tr>
-                                                <td style="font-family:'Open Sans', Arial, sans-serif; font-size:12px; line-height:15px; color:#0d1121;" valign="top" align="left">
-                                                    <img src="http://sun-sun33.com/wordpress/wp-content/themes/sun-sun/image/common/header/logo.png" class="img-fluid" alt="ぬか天国 Sun燦 淡路島の酵素風呂">  
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                            <!--//Header section-->
-                            <!--Content Text Section-->
-                            <tr>
-                                <td style="padding:35px 70px 30px;" class="em_padd" valign="top" bgcolor="#fff" align="center">
-                                    <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
-                                        <tbody>
-                                            <tr>
-                                                <td style="font-family:'Open Sans', Arial, sans-serif; font-size:18px; line-height:22px; color:#000 ; letter-spacing:2px; padding-bottom:12px; display:flex" valign="top" align="center">
-                                                    <img src="http://booking.sun-sun33.com/sunsun/imgs/icons/success.png" style="max-width: 50px">
-                                                    <div style="margin-left: 15px"> Order success</div>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td class="em_h20" style="font-size:0px; line-height:0px; height:25px;" height="25">&nbsp;</td>
-                                                <!--—this is space of 25px to separate two paragraphs ---->
-                                            </tr>
-                                            <tr>
-                                                <td style="font-family:'Open Sans', Arial, sans-serif; font-size:16px; line-height:30px; color:#000;" valign="top" align="center">Thank you for choosing us.</td>
-                                            </tr>
-                                            <tr>
-                                                <td style="font-size:0px; line-height:0px; height:15px;" height="15">&nbsp;</td>
-                                                <!--—this is space of 15px to separate two paragraphs ---->
-                                            </tr>
-                                            <tr>
-                                                <td style="font-family:'Open Sans', Arial, sans-serif; font-size:18px; line-height:22px; color:#000; letter-spacing:2px; padding-bottom:12px;" valign="top" align="center">BookingID: #201912250004</td>
-                                            </tr>
-                                            <tr>
-                                                <td style="font-family:'Open Sans', Arial, sans-serif; font-size:18px; line-height:22px; color:#000 ; letter-spacing:2px; padding-bottom:12px;" valign="top" align="center"> If you have any question, give us a call at</td>
-                                            </tr>
-                                            <tr>
-                                                <td style="font-family:'Open Sans', Arial, sans-serif; font-size:18px; line-height:22px; color:#000 ; letter-spacing:2px; padding-bottom:12px;" valign="top" align="center">90-1790-1357</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-
-                            <!--//Content Text Section-->
-                            <!--Footer Section-->
-                            <tr>
-                                <td style="padding:38px 30px;" class="em_padd" valign="top" bgcolor="#f6f7f8" align="center">
-                                    <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
-                                        <tbody>
-                                            <tr>
-                                                <td style="font-family:'Open Sans', Arial, sans-serif; font-size:11px; line-height:18px; color:#999999;" valign="top" align="center"><a href="#" target="_blank" style="color:#999999; text-decoration:underline;">PRIVACY STATEMENT</a> | <a href="#" target="_blank" style="color:#999999; text-decoration:underline;">TERMS OF SERVICE</a> | <a href="#" target="_blank" style="color:#999999; text-decoration:underline;">RETURNS</a>
-                                                    <br> COPYRIGHT © 2019 ぬか天国 Sun燦 all rights reserved.
-                                                    <br> If you do not wish to receive any further emails from us, please <a href="#" target="_blank" style="text-decoration:none; color:#999999;">unsubscribe</a></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="em_hide" style="line-height:1px;min-width:700px;background-color:#ffffff;"><img alt="" src="images/spacer.gif" style="max-height:1px; min-height:1px; display:block; width:700px; min-width:700px;" width="700" border="0" height="1"></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <div class="em_hide" style="white-space: nowrap; display: none; font-size:0px; line-height:0px;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div>
+<body class="em_body">
+    <div class="head_img">
+        <img src="http://sun-sun33.com/wordpress/wp-content/themes/sun-sun/image/common/header/logo.png" class="img-fluid_head" alt="ぬか天国 Sun燦 淡路島の酵素風呂">  
+    </div>
+    <div class="order_status">
+        <img src="http://booking.sun-sun33.com/sunsun/imgs/icons/success.png" class="order_status_image">
+        <div class="order_status_title">
+            <div>
+                Order success
+            </div>
+        </div>
+    </div>
+    <div class="content_block">
+        <div class="content_text">
+            Thank you for choosing us.
+        </div>
+        <div class="content_text">
+            BookingID: #201912250004
+        </div>
+        <div class="content_text">
+            If you have any question, give us a call at
+        </div>
+        <div class="content_text">
+            90-1790-1357
+        </div>
+    </div>
+    
+<div class="container-fluid text-center bg-color-01 pt-3 pb-1 footer">
+    <p class="fz-1_2 text-white d-none d-md-block">COPYRIGHT © 2019 ぬか天国 Sun燦 all rights reserved.</p>
+</div>
 </body>
-
 </html>
