cadd33ec90e98687827f4f16afd789317eff1030
MinhTu <minhtu.bk14@gmail.com>
2019-12-27 11:42:19 +0700

    add auth

--- routes/web.php
+++ routes/web.php
@@ -165,73 +165,80 @@ Route::get('/null_clear', function () {
 //     echo "Reset done!";
 // });
 
-Route::middleware('begin.auth')->namespace('Sunsun\Front')->group(function (){
-    Route::get('/main', function (){
-        return view('sunsun.front.main');
-    })->name('main');
-    Route::get('/thanks', function (){
 
-    })->name('thanks');
+Route::group(['middleware'=>'begin.auth'], function(){
+    Route::namespace('Sunsun\Front')->group(function (){
+        Route::get('/main', function (){
+            return view('sunsun.front.main');
+        })->name('main');
+        Route::get('/thanks', function (){
+    
+        })->name('thanks');
+    
+        Route::get('/', function () {
+            return view('sunsun.front.index');
+        })->name('home');
+    
+        Route::get('/booking',['as' => '.booking', 'uses' => 'BookingController@booking']);
+        Route::any('/confirm',['as' => '.confirm', 'uses' => 'BookingController@confirm']);
+        Route::any('/payment',['as' => '.payment', 'uses' => 'BookingController@payment']);
+        Route::post('/complete',['as' => '.complete', 'uses' => 'BookingController@complete']);
+        Route::post('/make_payment',['as' => '.make_payment', 'uses' => 'BookingController@make_payment']);
+    
+    
+    
+        Route::post('/get_service',['as' => '.get_service', 'uses' => 'BookingController@get_service']);
+    
+        Route::post('/get_free_room',['as' => '.get_free_room', 'uses' => 'BookingController@get_free_room']);
+    
+    
+        Route::post('/get_time_room',['as' => '.get_time_room', 'uses' => 'BookingController@get_time_room']);
+        Route::post('/book_room',['as' => '.book_room', 'uses' => 'BookingController@get_time_room']);
+        Route::post('/book_time_room_wt',['as' => '.book_time_room_wt', 'uses' => 'BookingController@book_time_room_wt']);
+        Route::post('/book_time_room_pet',['as' => '.book_time_room_pet', 'uses' => 'BookingController@book_time_room_pet']);
+        Route::post('/add_new_booking',['as' => '.add_new_booking', 'uses' => 'BookingController@add_new_booking']);
+        Route::post('/save_booking',['as' => '.add_new_booking', 'uses' => 'BookingController@save_booking']);
+    
+    });
 
-    Route::get('/', function () {
-        return view('sunsun.front.index');
-    })->name('home');
+    // Auth
+    Route::namespace('Sunsun\Auth')->group(function (){
+        Route::middleware('begin.auth')->get('/register', ['as' => 'register',  'uses' => 'MsUserController@register']);
+        Route::middleware('begin.auth')->get('/login', ['as' => 'login',  'uses' => 'LoginController@showLoginForm']);
+        Route::get('/admin/login', ['as' => 'admin-login',  'uses' => 'LoginController@showLoginForm']);
+        Route::post('/login', ['as' => 'auth', 'uses' => 'LoginController@login']);
+        Route::post('/login-admin', ['as' => 'auth-admin', 'uses' => 'AdminLoginController@login']);
+        Route::get('/logout', ['as' => 'logout', 'uses' => 'LoginController@logout']);
+        Route::Post('/create', ['as' => '.create',  'uses' => 'MsUserController@create']);
+    });
+    // user
+    Route::middleware('auth')->namespace('Sunsun\Auth')->group(function (){
+        Route::get('/edit', ['as' => '.edit',  'uses' => 'AuthUserController@edit']);
+        Route::Post('/edit', ['as' => '.upload',  'uses' => 'AuthUserController@edit']);
+        Route::get('/changepassword', ['as' => '.password',  'uses' => 'AuthUserController@changepassword']);
+        Route::post('/changepassword', ['as' => '.changepassword',  'uses' => 'AuthUserController@changepassword']);
+    });
 
-    Route::get('/booking',['as' => '.booking', 'uses' => 'BookingController@booking']);
-    Route::any('/confirm',['as' => '.confirm', 'uses' => 'BookingController@confirm']);
-    Route::any('/payment',['as' => '.payment', 'uses' => 'BookingController@payment']);
-    Route::post('/complete',['as' => '.complete', 'uses' => 'BookingController@complete']);
-    Route::post('/make_payment',['as' => '.make_payment', 'uses' => 'BookingController@make_payment']);
+    // Admin
+    Route::middleware('auth')->prefix('admin')->name('admin')->namespace('Sunsun\Admin')->group(function (){
+        Route::get('/',['as' => '.index', 'uses' => 'AdminController@index']);
+        Route::get('/day',['as' => '.day', 'uses' => 'AdminController@day']);
+        Route::get('/weekly',['as' => '.weekly', 'uses' => 'AdminController@weekly']);
+        Route::get('/monthly',['as' => '.monthly', 'uses' => 'AdminController@monthly']);
+        Route::get('/setting',['as' => '.setting', 'uses' => 'SettingController@setting']);
+        Route::post('/get_setting_type',['as' => '.get_setting_type', 'uses' => 'SettingController@get_setting_type']);
+        Route::post('/get_setting_kubun_type',['as' => '.get_setting_kubun_type', 'uses' => 'SettingController@get_setting_kubun_type']);
+        Route::post('/update_setting_kubun_type',['as' => '.update_setting_kubun_type', 'uses' => 'SettingController@update_setting_kubun_type']);
+        Route::post('/update_setting_sort_no',['as' => '.update_setting_sort_no', 'uses' => 'SettingController@update_setting_sort_no']);
+        Route::delete('/delete_setting_kubun_type',['as' => '.delete_setting_kubun_type', 'uses' => 'SettingController@delete_setting_kubun_type']);
 
-
-
-    Route::post('/get_service',['as' => '.get_service', 'uses' => 'BookingController@get_service']);
-
-    Route::post('/get_free_room',['as' => '.get_free_room', 'uses' => 'BookingController@get_free_room']);
-
-
-    Route::post('/get_time_room',['as' => '.get_time_room', 'uses' => 'BookingController@get_time_room']);
-    Route::post('/book_room',['as' => '.book_room', 'uses' => 'BookingController@get_time_room']);
-    Route::post('/book_time_room_wt',['as' => '.book_time_room_wt', 'uses' => 'BookingController@book_time_room_wt']);
-    Route::post('/book_time_room_pet',['as' => '.book_time_room_pet', 'uses' => 'BookingController@book_time_room_pet']);
-    Route::post('/add_new_booking',['as' => '.add_new_booking', 'uses' => 'BookingController@add_new_booking']);
-    Route::post('/save_booking',['as' => '.add_new_booking', 'uses' => 'BookingController@save_booking']);
-
+        Route::post('/edit_booking',['as' => '.edit_booking', 'uses' => 'AdminController@edit_booking']);
+        // Route::post('/booking_history',['as' => '.booking_history', 'uses' => 'AdminController@booking_history']);
+        Route::post('/show_history',['as' => '.show_history', 'uses' => 'AdminController@show_history']);
+        Route::post('/update_booking',['as' => '.update_booking', 'uses' => 'AdminController@update_booking']);
+    });
 });
 
-// Auth
-Route::namespace('Sunsun\Auth')->group(function (){
-    Route::get('/register', ['as' => 'register',  'uses' => 'MsUserController@register']);
-    Route::get('/login', ['as' => 'login',  'uses' => 'LoginController@showLoginForm']);
-    Route::get('/admin/login', ['as' => 'admin-login',  'uses' => 'LoginController@showLoginForm']);
-    Route::post('/login', ['as' => 'auth', 'uses' => 'LoginController@login']);
-    Route::post('/login-admin', ['as' => 'auth-admin', 'uses' => 'AdminLoginController@login']);
-    Route::get('/logout', ['as' => 'logout', 'uses' => 'LoginController@logout']);
-    Route::Post('/create', ['as' => '.create',  'uses' => 'MsUserController@create']);
-});
-// user
-Route::middleware('auth')->namespace('Sunsun\Auth')->group(function (){
-    Route::get('/edit', ['as' => '.edit',  'uses' => 'AuthUserController@edit']);
-    Route::Post('/edit', ['as' => '.upload',  'uses' => 'AuthUserController@edit']);
-    Route::get('/changepassword', ['as' => '.password',  'uses' => 'AuthUserController@changepassword']);
-    Route::post('/changepassword', ['as' => '.changepassword',  'uses' => 'AuthUserController@changepassword']);
-});
-// Admin
-Route::middleware('auth', 'can:admin')->prefix('admin')->name('admin')->namespace('Sunsun\Admin')->group(function (){
-    Route::get('/',['as' => '.index', 'uses' => 'AdminController@index']);
-    Route::get('/day',['as' => '.day', 'uses' => 'AdminController@day']);
-    Route::get('/weekly',['as' => '.weekly', 'uses' => 'AdminController@weekly']);
-    Route::get('/monthly',['as' => '.monthly', 'uses' => 'AdminController@monthly']);
-    Route::get('/setting',['as' => '.setting', 'uses' => 'SettingController@setting']);
-    Route::post('/get_setting_type',['as' => '.get_setting_type', 'uses' => 'SettingController@get_setting_type']);
-    Route::post('/get_setting_kubun_type',['as' => '.get_setting_kubun_type', 'uses' => 'SettingController@get_setting_kubun_type']);
-    Route::post('/update_setting_kubun_type',['as' => '.update_setting_kubun_type', 'uses' => 'SettingController@update_setting_kubun_type']);
-    Route::post('/update_setting_sort_no',['as' => '.update_setting_sort_no', 'uses' => 'SettingController@update_setting_sort_no']);
-    Route::delete('/delete_setting_kubun_type',['as' => '.delete_setting_kubun_type', 'uses' => 'SettingController@delete_setting_kubun_type']);
 
-    Route::post('/edit_booking',['as' => '.edit_booking', 'uses' => 'AdminController@edit_booking']);
-    // Route::post('/booking_history',['as' => '.booking_history', 'uses' => 'AdminController@booking_history']);
-    Route::post('/show_history',['as' => '.show_history', 'uses' => 'AdminController@show_history']);
-    Route::post('/update_booking',['as' => '.update_booking', 'uses' => 'AdminController@update_booking']);
-});
+
 
