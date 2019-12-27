b91e82c8a72ad7355a91d6fc4123c92a9b4ed954
MinhTu <minhtu.bk14@gmail.com>
2019-12-27 09:39:48 +0700

    add auth

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,16 @@ use Illuminate\Http\Request;
         echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
     }
  });
+ Route::middleware('begin.auth')->get('/aaa', function () {
+     return "aaa";
+ });
+ Route::middleware(['begin.auth'])->group(function () {
+    //All the routes are placed in here
+    Route::get('/bbb', function () {
+    
+        echo "bbb";
+     });
+});
 // Route::get('/demo_lock', function () {
 
 
