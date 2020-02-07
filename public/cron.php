bf8083cf6e98f01386fb0013316f59848b325984
MinhTu <minhtu.bk14@gmail.com>
2020-02-07 16:42:13 +0700

    add php

--- public/cron.php
+++ public/cron.php
@@ -6,6 +6,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
+exec("alias php=\'/usr/local/php7.3/bin/php\'");
 $status = $kernel->handle(
     $input = new Symfony\Component\Console\Input\ArrayInput(['command' => 'reminder:cron']),
     new Symfony\Component\Console\Output\ConsoleOutput
