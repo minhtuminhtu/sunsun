56e32bb05b72fdacee63232e556ab4b904a0b60a
MinhTu <minhtu.bk14@gmail.com>
2020-02-07 16:15:11 +0700

    test cron

---
+++ app/Console/Commands/ReminderCron.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class ReminderCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reminder:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Demo:Cron Cummand Run successfully!');
+        Log::debug('abc');
+    }
+}
