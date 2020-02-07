9e102ff8434b9b92b6ef1940644eb2c291bea3c5
MinhTu <minhtu.bk14@gmail.com>
2020-02-07 16:34:23 +0700

    test cron

--- public/cron.php
+++ public/cron.php
@@ -1,4 +1,16 @@
 <?php
-    exec("alias php=\'/usr/local/php7.3/bin/php\'");
-    exec("php artisan reminder:cron 1>> /dev/null 2>&1");
-?>
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+$status = $kernel->handle(
+    $input = new Symfony\Component\Console\Input\ArrayInput(['command' => 'reminder:cron']),
+    new Symfony\Component\Console\Output\ConsoleOutput
+);
+
+$kernel->terminate($input, $status);
+
+exit($status);
