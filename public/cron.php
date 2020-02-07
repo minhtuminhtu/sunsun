dd7b9b5b8c746cba5865ca49ba1d05f1cbbfaf25
MinhTu <minhtu.bk14@gmail.com>
2020-02-07 17:19:12 +0700

    change cron

--- public/cron.php
+++ public/cron.php
@@ -1,3 +1,3 @@
 <?php
     exec("alias php=\'/usr/local/php7.3/bin/php\'");
-    exec ('php /home/users/2/main.jp-26545197071588c/web/booking/public/cron.php');
+    exec ('php artisan reminder:cron');
