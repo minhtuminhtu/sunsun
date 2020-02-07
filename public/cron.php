ef376f8a95e1564e0b3ef47e47cd90a42fbf770d
MinhTu <minhtu.bk14@gmail.com>
2020-02-07 17:33:59 +0700

    change cron

--- public/cron.php
+++ public/cron.php
@@ -1,2 +1,2 @@
 <?php
-    echo exec("/usr/local/php7.3/bin/php artisan reminder:cron");
+    echo exec("/usr/local/php7.3/bin/php /home/users/2/main.jp-26545197071588c/web/booking/artisan reminder:cron");
