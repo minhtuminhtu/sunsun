6635aa2c6aa7cfca54a7a807b5049491448d34a9
MinhTu <minhtu.bk14@gmail.com>
2020-02-11 17:24:02 +0700

    remove auth

--- app/Http/Middleware/BeginAuth.php
+++ app/Http/Middleware/BeginAuth.php
@@ -20,21 +20,21 @@ class BeginAuth
     {
         $bookCon = new BookingController();
         Session::put("date_holiday",$bookCon->get_free_holiday());
-        $AUTH_USER = 'sunsunad';
-        $AUTH_PASS = '123456';
-        header('Cache-Control: no-cache, must-revalidate, max-age=0');
-        $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
-        $is_not_authenticated = (
-            !$has_supplied_credentials ||
-            $_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
-            $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
-        );
-        if ($is_not_authenticated) {
-            header('HTTP/1.1 200 Authorization Required');
-            header('WWW-Authenticate: Basic realm="Access denied"');
-            echo "Login issues. Access denied!";
-            exit;
-        }
+//        $AUTH_USER = 'sunsunad';
+//        $AUTH_PASS = '123456';
+//        header('Cache-Control: no-cache, must-revalidate, max-age=0');
+//        $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+//        $is_not_authenticated = (
+//            !$has_supplied_credentials ||
+//            $_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
+//            $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
+//        );
+//        if ($is_not_authenticated) {
+//            header('HTTP/1.1 200 Authorization Required');
+//            header('WWW-Authenticate: Basic realm="Access denied"');
+//            echo "Login issues. Access denied!";
+//            exit;
+//        }
         return $next($request);
         // return Auth::onceBasic() ?: $next($request);
 
