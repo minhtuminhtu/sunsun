a2217390615bff18afba915281dfbc9b2cf4bbe8
MinhTu <minhtu.bk14@gmail.com>
2020-02-14 15:10:28 +0700

    add breadcrumb

--- resources/views/sunsun/admin/layouts/breadcrumb.blade.php
+++ resources/views/sunsun/admin/layouts/breadcrumb.blade.php
@@ -1,4 +1,15 @@
-<p class="breadc"><a href="/" class="breadc-link">ぬか天国 Sun燦 トップページ</a> &#160;&#160;&gt;&#160; @yield('title')</p>
+<p class="breadc">
+    <span class="">
+        <a href="/" class="breadc-link">ホーム</a> &#160;&#160;&gt;&#160; @yield('title')
+    </span>
+    <span class="">
+        @if(Auth::check())
+            {{ isset(Auth::user()->username) ? Auth::user()->username:'' }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="breadc-link" href="{{ route('logout') }}">@lang('auth.logout')</a>
+        @else
+            <a class="breadc-link" href="{{ route('login') }}">@lang('auth.login')</a>
+        @endif
+    </span>
+</p>
 <div class="main-body-head text-center mb-3">
     <h1 class="title-menu text-left">@yield('title')</h1>
 </div>
--- resources/views/sunsun/front/layouts/header.blade.php
+++ resources/views/sunsun/front/layouts/header.blade.php
@@ -71,13 +71,21 @@
             </ul>
         </nav>
     </div>
-    
-    @if(Auth::check())
-        <p class="breadc">{{ isset(Auth::user()->username) ? Auth::user()->username:'' }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="breadc-link" href="{{ route('logout') }}">@lang('auth.logout')</a></p>
-    @else
-        <p class="breadc"><a class="breadc-link" href="{{ route('login') }}">@lang('auth.login')</a></p>
-    @endif
-    <p class="breadc"><a href="/" class="breadc-link">ホーム</a> &#160;&#160;&gt;&#160; @yield('page_title')</p>
+
+    <p class="breadc">
+        <span class="">
+            <a href="/" class="breadc-link">ホーム</a> &#160;&#160;&gt;&#160; @yield('page_title')
+        </span>
+        <span class="">
+            @if(Auth::check())
+                {{ isset(Auth::user()->username) ? Auth::user()->username:'' }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="breadc-link" href="{{ route('logout') }}">@lang('auth.logout')</a>
+            @else
+                <a class="breadc-link" href="{{ route('login') }}">@lang('auth.login')</a>
+            @endif
+        </span>
+    </p>
+
+
     <div class="main-body-head text-center mb-3">
         <h1 class="title-menu text-left">@yield('page_title')</h1>
     </div>
