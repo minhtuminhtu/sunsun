16e6adbb1025169e73048d9a38fa9355436724e3
MinhTu <minhtu.bk14@gmail.com>
2020-03-10 16:45:20 +0700

    fix ssl

--- resources/views/sunsun/front/layouts/header.blade.php
+++ resources/views/sunsun/front/layouts/header.blade.php
@@ -4,48 +4,48 @@
             <div class="col-2 mt-4">
                 <h1>
                     <a href="http://sun-sun33.com/">
-                        <img src="/sunsun/imgs/logo.png" class="img-fluid" alt="ぬか酸素 Sun燦 淡路島の酵素風呂">
+                        <img src="{{ asset('sunsun/imgs/logo.png') }}" class="img-fluid" alt="ぬか酸素 Sun燦 淡路島の酵素風呂">
                     </a>
                 </h1>
             </div>
             <!-- End of col -->
             <div class="col-10">
                 <div class="d-flex justify-content-end text-right">
-                    <div><a href="https://sun33.thebase.in/"><img src="/sunsun/imgs/btn-shop.png" alt="ネットショップはこちら" class="img-fluid"></a></div>
-                    <div><a href="http://sun-sun33.com/archives/category/voice"><img src="/sunsun/imgs/btn-voice.png" alt="お客様の声" class="img-fluid"></a></div>
-                    <div><a href="http://sun-sun33.com/shop"><img src="/sunsun/imgs/btn-access.png" alt="アクセス情報" class="img-fluid"></a></div>
+                    <div><a href="https://sun33.thebase.in/"><img src="{{ asset('sunsun/imgs/btn-shop.png') }}" alt="ネットショップはこちら" class="img-fluid"></a></div>
+                    <div><a href="http://sun-sun33.com/archives/category/voice"><img src="{{ asset('sunsun/imgs/btn-voice.png') }}" alt="お客様の声" class="img-fluid"></a></div>
+                    <div><a href="http://sun-sun33.com/shop"><img src="{{ asset('sunsun/imgs/btn-access.png') }}" alt="アクセス情報" class="img-fluid"></a></div>
                 </div>
                 <!-- End of row -->
 
                 <div class="d-flex justify-content-end text-right pb-3 mt-5">
                     <div class="pr-3 pr-lg-5 pb-3 border border-top-0 border-right-0 border-left-0 border-dashed border-brown">
                         <a href="http://sun-sun33.com/">
-                            <img src="/sunsun/imgs/nav1.png" alt="トップページへ" class="img-fluid">
+                            <img src="{{ asset('sunsun/imgs/nav1.png') }}" alt="トップページへ" class="img-fluid">
                         </a>
                     </div>
                     <div class="pr-3 pr-lg-5 pb-3 border border-top-0 border-right-0 border-left-0 border-dashed border-brown">
                         <a href="http://sun-sun33.com/about">
-                            <img src="/sunsun/imgs/nav2.png" alt="酵素風呂とは？" class="img-fluid">
+                            <img src="{{ asset('sunsun/imgs/nav2.png') }}" alt="酵素風呂とは？" class="img-fluid">
                         </a>
                     </div>
                     <div class="pr-3 pr-lg-5 pb-3 border border-top-0 border-right-0 border-left-0 border-dashed border-brown">
                         <a href="http://sun-sun33.com/menu">
-                            <img src="/sunsun/imgs/nav3.png" alt="メニュー・ご利用料金" class="img-fluid">
+                            <img src="{{ asset('sunsun/imgs/nav3.png') }}" alt="メニュー・ご利用料金" class="img-fluid">
                         </a>
                     </div>
                     <div class="pr-3 pr-lg-5 pb-3 border border-top-0 border-right-0 border-left-0 border-dashed border-brown">
                         <a href="http://sun-sun33.com/flow">
-                            <img src="/sunsun/imgs/nav4.png" alt="酵素風呂の入り方" class="img-fluid">
+                            <img src="{{ asset('sunsun/imgs/nav4.png') }}" alt="酵素風呂の入り方" class="img-fluid">
                         </a>
                     </div>
                     <div class="pr-3 pr-lg-5 pb-3 border border-top-0 border-right-0 border-left-0 border-dashed border-brown">
                         <a href="http://sun-sun33.com/shop">
-                            <img src="/sunsun/imgs/nav5.png" alt="お店のご紹介" class="img-fluid">
+                            <img src="{{ asset('sunsun/imgs/nav5.png') }}" alt="お店のご紹介" class="img-fluid">
                         </a>
                     </div>
                     <div class=" pb-3 border border-top-0 border-right-0 border-left-0 border-dashed border-brown">
                         <a href="http://sun-sun33.com/contact">
-                            <img src="/sunsun/imgs/nav6.png" alt="お問い合わせ" class="img-fluid">
+                            <img src="{{ asset('sunsun/imgs/nav6.png') }}" alt="お問い合わせ" class="img-fluid">
                         </a>
                     </div>
                 </div>
